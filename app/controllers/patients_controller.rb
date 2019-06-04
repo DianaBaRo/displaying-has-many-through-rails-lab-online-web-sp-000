a4bc4993f72cca80056828090c82fa0944cb4755
Diana Badas <diana.badas@gmail.com>
2019-06-04 11:06:48 +0000

    Automatically backed up by Learn

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,9 +1,10 @@
 class PatientsController < ApplicationController
-  def show
-    @patient = Patient.find_by(params[:id])
-  end
 
   def index
     @patients = Patient.all
+  end
+
+  def show
+    @patient = Patient.find_by(params[:id])
   end
 end
