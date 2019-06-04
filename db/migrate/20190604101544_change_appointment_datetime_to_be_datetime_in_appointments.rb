421a8a46f4494dd048e4dc2dbe74bdd7ceb61494
Diana Badas <diana.badas@gmail.com>
2019-06-04 10:19:31 +0000

    Automatically backed up by Learn

--- db/migrate/20190604101544_change_appointment_datetime_to_be_datetime_in_appointments.rb
+++ db/migrate/20190604101544_change_appointment_datetime_to_be_datetime_in_appointments.rb
@@ -1,5 +1,5 @@
 class ChangeAppointmentDatetimeToBeDatetimeInAppointments < ActiveRecord::Migration[5.0]
   def change
-    change_column :appointments, :string, :datetime
+    change_column :appointments, :appointment_datetime, :datetime
   end
 end
